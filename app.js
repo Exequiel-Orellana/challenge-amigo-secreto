032ae19869d02be243df5bd620b6cbb895c1a57a
Exequiel-Orellana <andres.o.caileo@gmail.com>
2025-03-08 13:49:21 -0300

    Crea la funcion para agregar un amigo

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 let amigos = []; 
+
+// Función para agregar un amigo
+function agregarAmigo(event) {
+    event.preventDefault(); 
+
+    const input = document.getElementById('amigo'); 
+    const nombre = input.value.trim(); 
+
+    
+    if (!nombre) {  
+        alert("Por favor, inserte un nombre.");
+        input.focus(); 
+        return; 
+    }
+
+   
+    if (amigos.includes(nombre)) {
+        alert("Ese nombre ya ha sido agregado.");
+        input.value = ""; 
+        input.focus();
+        return;
+    }
+
+    
+    amigos.push(nombre);
+    input.value = ""; 
+    mostrarListaAmigos();
+}
